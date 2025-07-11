e8c1f56f284149aebe74b5a52fcea73f2c70da72
kognitivamitsuo <mitsuo.kaneko@yahoo.com.br>
2025-07-11 12:14:09 -0300

    Update feedbackService.js

--- src/services/feedbackService.js
+++ src/services/feedbackService.js
@@ -1,5 +1,14 @@
-const pool = require('../models/db');
+'use strict';
 
+// Remover qualquer uso de alias e substituir por caminhos relativos
+const pool = require('../models/db');  // Caminho relativo
+
+/**
+ * Salva o feedback no banco de dados
+ * @param {number} userId - ID do usuário
+ * @param {string} feedback - Feedback dado pelo usuário
+ * @param {string} comentario - Comentário adicional do usuário
+ */
 async function salvarFeedback(userId, feedback, comentario) {
   const query = 'INSERT INTO feedbacks (user_id, feedback, comentario) VALUES ($1, $2, $3)';
   await pool.query(query, [userId, feedback, comentario]);
